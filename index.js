7b58aa6d9e25236bd3710c75ab7d3264682f4949
zaidhanif1 <zah00005@mix.wvu.edu>
2024-06-15 20:12:48 -0400

    Done

--- index.js
+++ index.js
@@ -1,7 +1,46 @@
-let totalScore = 0
+let totalScoreLeft = 0
 
 
-function add1()
+function add1left()
 {
-    document.getElementById("leftScore").textContent = totalScore + 1
+    totalScoreLeft += 1
+    document.getElementById("leftScore").textContent = totalScoreLeft
+}
+function add2left()
+{
+    totalScoreLeft += 2
+    document.getElementById("leftScore").textContent = totalScoreLeft
+}
+function add3left()
+{
+    totalScoreLeft += 3
+    document.getElementById("leftScore").textContent = totalScoreLeft
+}
+
+
+
+let totalScoreRight = 0
+function add1right()
+{
+    totalScoreRight += 1
+    document.getElementById("rightScore").textContent = totalScoreRight
+}
+function add2right()
+{
+    totalScoreRight += 2
+    document.getElementById("rightScore").textContent = totalScoreRight
+}
+function add3right()
+{
+    totalScoreRight += 3
+    document.getElementById("rightScore").textContent = totalScoreRight
+}
+
+function reset()
+{
+    document.getElementById("rightScore").textContent = 0
+    document.getElementById("leftScore").textContent = 0
+    totalScoreRight= 0
+    totalScoreLeft = 0
+
 }
